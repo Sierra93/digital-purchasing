057483d8778c703a63cc703b3aa0797d449da4dc
Ilya Kabanov <9848333@gmail.com>
2018-08-07 12:32:19 +0200

    Standard template

---
+++ DigitalPurchasing.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DigitalPurchasing.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
