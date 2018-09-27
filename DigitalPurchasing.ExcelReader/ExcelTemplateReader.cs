4731e8dd879b0b535a697134d9f09ecd4538c86a
Ilya Kabanov <9848333@gmail.com>
2018-09-27 21:04:53 +0200

    Fix deploy

--- DigitalPurchasing.ExcelReader/ExcelTemplateReader.cs
+++ DigitalPurchasing.ExcelReader/ExcelTemplateReader.cs
@@ -41,29 +41,31 @@ namespace DigitalPurchasing.ExcelReader
 
         public byte[] Build()
         {
-            var excel = new ExcelPackage();
-            var ws = excel.Workbook.Worksheets.Add("Template");
-            ws.Cells[1, 1].Value = "Категория";
-            ws.Cells[1, 2].Value = "Код";
-            ws.Cells[1, 3].Value = "Название";
-            ws.Cells[1, 4].Value = "Название Eng";
-            ws.Cells[1, 5].Value = "EИ";
-            ws.Cells[1, 6].Value = "EИ массы";
-            ws.Cells[1, 7].Value = "Масса 1 ЕИ, ЕИ массы";
-            ws.Cells[1, 8].Value = "Название ресурса";
-            ws.Cells[1, 9].Value = "Ресурс, 1 ЕИ ресурса";
-            ws.Cells[1, 10].Value = "ЕИ ресурса";
-            ws.Column(1).AutoFit();
-            ws.Column(2).AutoFit();
-            ws.Column(3).AutoFit();
-            ws.Column(4).AutoFit();
-            ws.Column(5).AutoFit();
-            ws.Column(6).AutoFit();
-            ws.Column(7).AutoFit();
-            ws.Column(8).AutoFit();
-            ws.Column(9).AutoFit();
-            ws.Column(10).AutoFit();
-            return excel.GetAsByteArray();
+            using (var excel = new ExcelPackage())
+            {
+                var ws = excel.Workbook.Worksheets.Add("Template");
+                ws.Cells[1, 1].Value = "Категория";
+                ws.Cells[1, 2].Value = "Код";
+                ws.Cells[1, 3].Value = "Название";
+                ws.Cells[1, 4].Value = "Название Eng";
+                ws.Cells[1, 5].Value = "EИ";
+                ws.Cells[1, 6].Value = "EИ массы";
+                ws.Cells[1, 7].Value = "Масса 1 ЕИ, ЕИ массы";
+                ws.Cells[1, 8].Value = "Название ресурса";
+                ws.Cells[1, 9].Value = "Ресурс, 1 ЕИ ресурса";
+                ws.Cells[1, 10].Value = "ЕИ ресурса";
+                ws.Column(1).AutoFit();
+                ws.Column(2).AutoFit();
+                ws.Column(3).AutoFit();
+                ws.Column(4).AutoFit();
+                ws.Column(5).AutoFit();
+                ws.Column(6).AutoFit();
+                ws.Column(7).AutoFit();
+                ws.Column(8).AutoFit();
+                ws.Column(9).AutoFit();
+                ws.Column(10).AutoFit();
+                return excel.GetAsByteArray();
+            }
         }
 
         public List<TemplateData> Read(string filePath)
