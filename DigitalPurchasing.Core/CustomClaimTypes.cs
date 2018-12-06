2df7acbbb59839d9e67e918e8477f315d347292d
Ilya Kabanov <9848333@gmail.com>
2018-12-06 13:20:08 +0100

    Company name notification

--- DigitalPurchasing.Core/CustomClaimTypes.cs
+++ DigitalPurchasing.Core/CustomClaimTypes.cs
@@ -3,5 +3,6 @@ namespace DigitalPurchasing.Core
     public class CustomClaimTypes
     {
         public static string CompanyId = "Custom:CompanyId";
+        public static string CompanyName = "Custom:CompanyName";
     }
 }
--- DigitalPurchasing.Web/Core/CustomUserClaimsPrincipalFactory.cs
+++ DigitalPurchasing.Web/Core/CustomUserClaimsPrincipalFactory.cs
@@ -1,6 +1,8 @@
-﻿using System.Security.Claims;
+using System;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using DigitalPurchasing.Core;
+using DigitalPurchasing.Core.Interfaces;
 using DigitalPurchasing.Models.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
@@ -9,17 +11,20 @@ namespace DigitalPurchasing.Web.Core
 {
     public class CustomUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<User, Role>
     {
+        private readonly ICompanyService _companyService;
+
         public CustomUserClaimsPrincipalFactory(
             UserManager<User> userManager,
             RoleManager<Role> roleManager,
-            IOptions<IdentityOptions> optionsAccessor)
-            : base(userManager, roleManager, optionsAccessor) {
-        }
+            IOptions<IdentityOptions> optionsAccessor,
+            ICompanyService companyService)
+            : base(userManager, roleManager, optionsAccessor) => _companyService = companyService;
 
         protected override async Task<ClaimsIdentity> GenerateClaimsAsync(User user)
         {
             var identity = await base.GenerateClaimsAsync(user);
             identity.AddClaim(new Claim(CustomClaimTypes.CompanyId, user.CompanyId.ToString("N")));
+            identity.AddClaim(new Claim(CustomClaimTypes.CompanyName, _companyService.GetByUser(user.Id).Name ?? string.Empty));
             return identity;
         }
     }
