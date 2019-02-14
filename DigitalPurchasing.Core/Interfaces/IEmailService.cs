46383500b3f76095432b66c9bb4c5f4084d0bcb4
Ilya Kabanov <9848333@gmail.com>
2019-02-14 13:21:44 +0100

    Emila service

--- DigitalPurchasing.Core/Interfaces/IEmailService.cs
+++ DigitalPurchasing.Core/Interfaces/IEmailService.cs
@@ -4,6 +4,12 @@ namespace DigitalPurchasing.Core.Interfaces
 {
     public interface IEmailService
     {
-        Task SendEmailAsync(string email, string subject, string htmlMessage);
+        Task SendEmailAsync(string email, string subject, string htmlMessage, ReplyTo replyTo = ReplyTo.Hello);
+    }
+
+    public enum ReplyTo
+    {
+        Hello,
+        App
     }
 }
--- DigitalPurchasing.Services/EmailService.cs
+++ DigitalPurchasing.Services/EmailService.cs
@@ -1,6 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using DigitalPurchasing.Core.Interfaces;
 
@@ -8,9 +7,41 @@ namespace DigitalPurchasing.Services
 {
     public class EmailService : IEmailService
     {
-        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        public Task SendEmailAsync(string email, string subject, string htmlMessage, ReplyTo replyTo = ReplyTo.Hello)
         {
-            return;
+            var client = new SmtpClient("smtp.mandrillapp.com", 587)
+            {
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential("PurchasingTool", "qbm_XHdv5CerLGRZJpYWfQ")
+            };
+
+            var mailMessage = new MailMessage
+            {
+                Sender = new MailAddress("donotreply@digitalpurchasing.com", "DigitalPurchasing.com"),
+                From = new MailAddress("donotreply@digitalpurchasing.com", "DigitalPurchasing.com"),
+            };
+
+            var defaultMailAddress = new MailAddress("hello@digitalpurchasing.com", "DigitalPurchasing.com");
+
+            switch (replyTo)
+            {
+                case ReplyTo.Hello:
+                    mailMessage.ReplyToList.Add(defaultMailAddress);
+                    break;
+                case ReplyTo.App:
+                    mailMessage.ReplyToList.Add(new MailAddress("app@digitalpurchasing.com", "DigitalPurchasing.com"));
+                    break;
+                default:
+                    mailMessage.ReplyToList.Add(defaultMailAddress);
+                    break;
+            }
+            
+            mailMessage.To.Add(email);
+            mailMessage.Body = htmlMessage;
+            mailMessage.Subject = subject;
+            mailMessage.IsBodyHtml = true;
+            client.Send(mailMessage);
+            return Task.FromResult(true);
         }
     }
 }
