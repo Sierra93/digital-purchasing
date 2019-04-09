ad6f1c1cb3a6b46942a8bf70078fedef929c04fa
Ilya Kabanov <9848333@gmail.com>
2019-04-09 13:50:05 +0200

    AnalysisCore optimizations p.2

--- DigitalPurchasing.Analysis2/Filters/VariantsItemQuantityFilter.cs
+++ DigitalPurchasing.Analysis2/Filters/VariantsItemQuantityFilter.cs
@@ -6,18 +6,28 @@ namespace DigitalPurchasing.Analysis2.Filters
 {
     public class VariantsItemQuantityFilter : VariantsFilter
     {
-        public override List<List<AnalysisData>> Filter(List<List<AnalysisData>> allVariants, IAnalysisContext context)
+        public override List<List<AnalysisData>> Filter(List<List<AnalysisData>> variants, IAnalysisContext context)
         {
             var customerItems = context.Customer.Items.ToDictionary(q => q.Id);
-            var v = allVariants
+
+            var fullVariants = variants.Where(q => q.All(w => w.Item.Quantity >= customerItems[w.Item.Id].Quantity)).ToList();
+
+            if (fullVariants.Count == variants.Count)
+            {
+                return fullVariants;
+            }
+
+            var partialVariants = variants.Except(fullVariants);
+            
+            var validPartialVariants = partialVariants
                 .Where(
                     w => w
                         .GroupBy(g=> g.Item.Id)
                         .Select(q => new { ItemId = q.Key, Qty = q.Sum(e => e.Item.Quantity) })
                         .All(r => r.Qty >= customerItems[r.ItemId].Quantity))
                 .ToList();
-
-            return v;
+            
+            return fullVariants.Union(validPartialVariants).ToList();
         }
     }
 }
