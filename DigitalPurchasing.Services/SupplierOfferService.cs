2bfc873f14b8043aa121685a1f2037205bc59eb7
eshangin <shangin.jekon@mail.ru>
2019-05-16 00:27:14 +0700

    trying to improve alg (F5)

--- DigitalPurchasing.Services/SupplierOfferService.cs
+++ DigitalPurchasing.Services/SupplierOfferService.cs
@@ -383,15 +383,16 @@ namespace DigitalPurchasing.Services
                                     item.PurchaseRequest.QuotationRequest.CompetitionList.SupplierOffers.Any(so => so.Id == soId)
                                select item).ToList();
 
-                Func<string, string> cleanupNomName = (nomName) => Regex.Replace(nomName, @"[^a-zA-Z0-9\p{IsCyrillic}\s]", "");
+                Func<string, string> cleanupNomName = (nomName) => Regex.Replace(nomName, @"[^a-zA-Z0-9\p{IsCyrillic}\s]", " ");
                 Func<string, string> leaveOnlyDigits = (str) => Regex.Replace(str, "[^0-9]", "");
+                Func<string, string> orderWords = (str) => string.Join(' ', str.Split(' ').OrderBy(w => w));
 
                 var unlinkedPrItems = prItems.Where(item => !soItems.Any(soItem => soItem.NomenclatureId == item.NomenclatureId))
                     .Select(item => new
                     {
                         item.Id,
                         item.NomenclatureId,
-                        RawName = cleanupNomName(item.RawName.ReplaceSpacesWithOneSpace()),
+                        RawName = cleanupNomName(item.RawName).ReplaceSpacesWithOneSpace().Trim(),
                         item.RawUomMatchId,
                         item.RawQty
                     });
@@ -401,15 +402,17 @@ namespace DigitalPurchasing.Services
                 var tolerance = 0.15;
 
                 var algResults = (from soItem in soItems.Where(_ => !_.NomenclatureId.HasValue)
-                                  let soItemName = cleanupNomName(soItem.RawName.ReplaceSpacesWithOneSpace())
+                                  let soItemName = cleanupNomName(soItem.RawName).ReplaceSpacesWithOneSpace().Trim()
                                   from prItem in unlinkedPrItems
                                   let maxNameLen = Math.Max(soItemName.Length, prItem.RawName.Length)
                                   let soDigits = leaveOnlyDigits(soItemName)
                                   let prDigits = leaveOnlyDigits(prItem.RawName)
                                   let sameUom = soItem.RawUomId == prItem.RawUomMatchId
-                                  let nameDistance = alg.Distance(soItemName, prItem.RawName)
+                                  let nameStr1 = orderWords(soItemName.ToLower())
+                                  let nameStr2 = orderWords(prItem.RawName.ToLower())
+                                  let nameDistance = alg.Distance(nameStr1, nameStr2)
                                   let digitsDistance = alg.Distance(soDigits, prDigits)
-                                  let qtyDiff = sameUom ? Math.Abs(prItem.RawQty - soItem.RawQty) / (10 * Math.Max(prItem.RawQty, soItem.RawQty)) : 0
+                                  let qtyDiff = sameUom ? Math.Abs(prItem.RawQty - soItem.RawQty) / (10 * Math.Max(prItem.RawQty, soItem.RawQty)) : 0.1m
                                   let completeDistance = (nameDistance + digitsDistance) / (2 * maxNameLen) + (double)qtyDiff
                                   select new
                                   {
@@ -418,12 +421,16 @@ namespace DigitalPurchasing.Services
                                       soDigits,
                                       prDigits,
                                       nameDistance,
+                                      nameStr1,
+                                      nameStr2,
                                       digitsDistance,
                                       qtyDiff,
                                       completeDistance
                                   }).ToList();
 
-                algResults = algResults.Where(el => el.completeDistance <= tolerance).OrderBy(el => el.completeDistance).ToList();
+                algResults = algResults
+                    //.Where(el => el.completeDistance <= tolerance)
+                    .OrderBy(el => el.completeDistance).ToList();
 
                 while (algResults.Any())
                 {
